478beabc341b3d7f1977ed9f144f9c52d9d1763d
song106639 <slf106639@gmail.com>
2023-02-03 20:49:46 +0800

    Initial commit

---
+++ src/test/java/com/song/schedulesystem/ScheduleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.song.schedulesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
